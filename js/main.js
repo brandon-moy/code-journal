aeb3a55535067ddac59662bd0b9d3aad456fda87
Brandon Moy <brandonkaghenmoy@gmail.com>
2022-09-12 23:48:44 +0000

    Started function to generate DOM tree that matches example entry

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ var $title = document.querySelector('#title');
 var $photoURL = document.querySelector('#img-url');
 var $notes = document.querySelector('#notes');
 var $form = document.querySelector('.entry-form');
+// var $entryList = document.querySelector('.entry-list');
 
 function updatePhoto(event) {
   $img.setAttribute('src', $photoURL.value);
@@ -20,6 +21,24 @@ function saveEntry(event) {
   $img.setAttribute('src', '../images/placeholder-image-square.jpg');
   $form.reset();
 }
+
+// function createEntryTree(object) {
+//   var $li = document.createElement('li');
+//   var $divRow = document.createElement('div');
+//   var $divColumn = document.createElement('div');
+//   $divRow.className = 'row';
+//   $divColumn.className = 'column-half';
+//   var $img = document.createElement('img');
+//   $img.className = 'journal-image';
+//   var $h4 = document.createElement('h4');
+//   $h4.className = 'journal-header';
+//   var $p = document.createElement('p');
+//   $p.className = 'journal-text';
+//   $entryList.appendChild($li);
+//   $li.appendChild($divRow);
+//   $divRow.appendChild($divColumn);
+//   $divColumn.appendChild($img);
+// }
 
 $photoURL.addEventListener('input', updatePhoto);
 $form.addEventListener('submit', saveEntry);
